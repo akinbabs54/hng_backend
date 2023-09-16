e4c9b8967f948043ffbaec333b8b707c0ee4aa9b
Akinola Babalola <81317105+akinbabs54@users.noreply.github.com>
2023-09-16 16:24:45 +0200

    Update app.py

--- task_2/app.py
+++ task_2/app.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
 db = SQLAlchemy(app)
+with app.app_context():
+    db.create_all()
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True)
