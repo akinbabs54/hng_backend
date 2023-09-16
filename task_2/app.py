2392c87a7f01be033b801976522495431591e324
Akinola Babalola <81317105+akinbabs54@users.noreply.github.com>
2023-09-16 19:34:13 +0200

    Update app.py

--- task_2/app.py
+++ task_2/app.py
@@ -15,7 +15,7 @@ db = SQLAlchemy(app)
 
 class Person(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(80), unique=True, nullable=False, unique=True)
+    name = db.Column(db.String(80), nullable=False, unique=True)
 
 # Create a new person
 @app.route('/api', methods=['POST'])
