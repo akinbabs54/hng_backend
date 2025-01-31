979e319be217d9dc1c800bd3c1ad2b3189179f81
akinbabs54 <akinolababalola665@gmail.com>
2025-01-31 19:06:04 +0000

    env used

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ app = FastAPI(
 @app.get("/myinfo")  # is a decorator that registers a GET request route. executes the function on link.  Define a GET endpoint at "/myinfo on the app"
 def get_info():
     return {
-        "email": "akinolababalola665@gmail.com",  
+        "email": ("EMAIL"),  
         "current_time": datetime.now(pytz.utc).isoformat(),  # Get current UTC time using pytz
-        "github_repo": "https://github.com/akinbabs54/hng_backend"  
+        "github_repo": ("GITHUB_REPO")  
     }
 
