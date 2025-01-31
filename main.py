cbf2f93f599e25c19144ce0597c9cef3401cf73a
akinbabs54 <akinolababalola665@gmail.com>
2025-01-31 21:45:45 +0000

    env

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI  # Import framwork itself
 from datetime import datetime  # import the date and time module
 import pytz  # this module handles the time zoning
 from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env file
+load_dotenv(dotenv_path='./.env')
 
 app = FastAPI(
     title="My API",
@@ -12,8 +16,8 @@ app = FastAPI(
 @app.get("/myinfo")  # is a decorator that registers a GET request route. executes the function on link.  Define a GET endpoint at "/myinfo on the app"
 def get_info():
     return {
-        "email": ("EMAIL"),  
+        "email": os.getenv("EMAIL"),  
         "current_time": datetime.now(pytz.utc).isoformat(),  # Get current UTC time using pytz
-        "github_repo": ("GITHUB_REPO")  
+        "github_repo": os.getenv("GITHUB_REPO")  
     }
 
