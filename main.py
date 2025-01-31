6b3e3b98f50753fc5b76fb1e4a0b0880dcc10996
akinbabs54 <akinolababalola665@gmail.com>
2025-01-31 22:22:25 +0000

    added cors

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI  # Import framwork itself
+from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime  # import the date and time module
 import pytz  # this module handles the time zoning
 from dotenv import load_dotenv
@@ -7,11 +9,20 @@ import os
 # Load environment variables from .env file
 load_dotenv(dotenv_path='./.env')
 
+
 app = FastAPI(
     title="My API",
     description="This is a sample FastAPI application.",
     version="1.0.0"
 )  # Create an instance of FastAPI
+
+# ✅ Add CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_methods=["GET"],  # Allowed HTTP methods
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.get("/myinfo")  # is a decorator that registers a GET request route. executes the function on link.  Define a GET endpoint at "/myinfo on the app"
 def get_info():
